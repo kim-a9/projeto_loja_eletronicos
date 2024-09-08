523dfa374cc2970cdba4d7124acd4abfecad7dae
Kimberly <175900673+kim-a9@users.noreply.github.com>
2024-09-08 19:51:51 -0300

    alteração na rota index e criação página cadastro

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 # Inicialização do aplicativo, olhar documentação Flask - Layout do projeto
 from flask import Flask
+from flask import render_template, request, redirect, url_for, flash
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deu bom!!!"
+    return render_template('index.html')
+
+@app.route("/cadastro", methods =['GET', 'POST'])
+def cadastro():
+#    if request.method == 'POST':
+#        flash('Produto cadastrado!')
+#        return redirect(url_for('index.html'))
+    return render_template('cadastro.html')
+
+
 
