52cba34b2a352a0ce725ad5fe2ed5b4edadd9f6b
kim-a9 <kimberlysantosalves@gmail.com>
2024-09-26 16:17:30 -0300

    template cadastro de produtos

--- forms.py
+++ forms.py
@@ -3,7 +3,8 @@ from wtforms import StringField, IntegerField, FloatField, SubmitField
 from wtforms.validators import DataRequired
 
 class CadastroForm(FlaskForm):
+    codigo = IntegerField('Código', validators=[DataRequired()])
     produto = StringField('Produto', validators=[DataRequired()])
+    categoria = StringField('Categoria', validators=[DataRequired()])
     quantidade = IntegerField('Quantidade', validators=[DataRequired()])
-    preco = FloatField('Preço', validators=[DataRequired()])
     submit = SubmitField('Cadastrar')
