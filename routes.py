200a00086b8f81a75cd6c743e317b250ade50a34
kim-a9 <kimberlysantosalves@gmail.com>
2024-10-03 17:08:48 -0300

    template editar e excluir, tratamento de erros

--- routes.py
+++ routes.py
@@ -16,28 +16,31 @@ def home():
 def cadastro():
     form = CadastroForm()
     if form.is_submitted() and form.validate():
-        # codigoprod = form.codigoprod.data
-        # produto = form.produto.data
-        # categoria = form.categoria.data
-        # quantidade = form.quantidade.data
+        try: 
+            codigoprod = form.codigoprod.data
+            produto = form.produto.data
+            categoria = form.categoria.data
+            quantidade = form.quantidade.data
 
-        # if CadastroProduto.query.filter_by(produto=produto).first(): 
-        #     flash('Produto já cadastrado')
-        #     return redirect(url_for(cadastro.html))
+            # if CadastroProduto.query.filter_by(produto=produto).first(): 
+            #     abort(400, message='Este produto já foi cadastrado.')
+            #     return redirect(url_for("main.cadastro"))
 
-        # cadastro = CadastroProduto(
-        #     codigoprod=codigoprod,
-        #     produto=produto,
-        #     categoria=categoria,
-        #     quantidade=quantidade
-        # )
+            # cadastro = CadastroProduto(
+            #     codigoprod=codigoprod,
+            #     produto=produto,
+            #     categoria=categoria,
+            #     quantidade=quantidade
+            # )
 
-        # db.session.add(cadastro)
-        # db.session.commit()
+            # db.session.add(cadastro)
+            # db.session.commit()
 
-        flash('Produto cadastrado com sucesso!')
-        return redirect(url_for('home'))
-    
+            flash('Produto cadastrado com sucesso!')
+            return redirect(url_for('home'))
+        except KeyError:
+            abort(400, message="Erro ao cadastrar produto. Por favor, verifique os campos preenchidos.")
+
     return render_template("cadastro.html", form=form)
 
 @bp.route("/pesquisa", methods=['GET', 'POST'])
@@ -46,11 +49,40 @@ def pesquisa():
     #     if request.method == 'POST': 
     #         produtos = CadastroProduto.query.all()
     #         if not produtos:
-    #             flash('Nenhum produto cadastrado ainda!')
-    #             return redirect(url_for('pesquisa'))
+    #             abort(400, message='Nenhum produto cadastrado ainda!')
+    #         return redirect(url_for('pesquisa'))
+    #     return render_template('pesquisa', produtos=produtos)
     # except KeyError:
     #     abort(404, description='Produto não encontrado. Verifique os campos preenchidos')
     return render_template('pesquisa.html')
+
+@bp.route("/editar_produto/", methods=["GET", "POST"])
+def editar_produto():
+    # produto = CadastroProduto.query.all()
+    # form = CadastroForm()
+    # if request.method == 'POST':
+    #     produto.codigoprod = form.codigoprod.data
+    #     produto.produto = form.produto.data
+    #     produto.categoria = form.categoria.data
+    #     produto.quantidade = form.quantidade.data
+
+    #     try:
+    #         db.session.commit()
+    #         flash('Produto editado!')
+    #         return redirect(url_for("main.pesquisa"))
+    #     except KeyError:
+    #         abort(400, message="Erro ao atualizar produtos. Por favor, verifique os campos preenchidos.")
+    #         return render_template("editar_produto.html", form=form, produto=produto)
+    return render_template("editar_produto.html", form=form, produto=produto)
+
+@bp.route("/excluir/", methods=["POST"])
+def excluir():
+    # produto = CadastroProduto.query.get()
+    # db.session.delete()
+    # db.session.commit()
+    # flash('Produto excluído do estoque!')
+    return redirect(url_for('pesquisa'))
+
 
 @bp.route("/venda", methods=['GET', 'POST'])
 def venda():
